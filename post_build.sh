86c571b565d8d8882a9105fc7cc1e2eb1dce7cb3
Peter Strömberg <pez@pezius.com>
2018-02-05 15:25:03 +0100

    Rename resources dir

--- post_build.sh
+++ post_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-RESOURCES_SRC_DIR='resources'
+ASSETS_SRC_DIR='assets'
 PLUGIN_DIR=$(node << EOF
     dict = $(<package.json);
     console.log(dict.skpm.main);
@@ -10,4 +10,4 @@ EOF
 RESOURCES_DIR="${PLUGIN_DIR}/Contents/Resources/"
 
 mkdir -p ${RESOURCES_DIR}
-cp -a "${RESOURCES_SRC_DIR}"/* ${RESOURCES_DIR}
+cp -a "${ASSETS_SRC_DIR}"/* ${RESOURCES_DIR}
